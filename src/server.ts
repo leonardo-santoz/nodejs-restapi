0314067560237c99b88803771699995481ba3d90
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2024-08-08 17:12:59 -0300

    02 - configurando o knex

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import fastify from 'fastify'
+import { knex } from './databse'
 
 const app = fastify()
+
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
+})
 
 app.listen({ port: 3333 }).then(() => {
   console.log('app is running')
