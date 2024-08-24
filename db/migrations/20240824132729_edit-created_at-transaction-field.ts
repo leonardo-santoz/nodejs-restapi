63a6b5a7f6120cd6032da5d5a2944526e8cf9169
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2024-08-24 10:34:21 -0300

    02 - realizando queries com knex

---
+++ db/migrations/20240824132729_edit-created_at-transaction-field.ts
@@ -0,0 +1,13 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable().alter()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.dropColumn('created_at')
+  })
+}
