e4d35b887536aa7fc2c8946eb2309d68f0c92e4a
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2024-08-27 06:53:53 -0300

    03 - validando a existência do cookie

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
+import cookie from '@fastify/cookie'
+
 import { env } from './env'
 import { transactionRoutes } from './routes/transactions'
 
 const app = fastify()
 
+app.register(cookie)
 app.register(transactionRoutes, { prefix: 'transactions' })
 
 app.listen({ port: env.PORT }).then(() => {
