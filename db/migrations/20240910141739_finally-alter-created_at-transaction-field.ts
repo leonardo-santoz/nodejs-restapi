2bc06c1ea7fb269306e8810bbc94fcd8baf957b5
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2024-09-10 11:26:05 -0300

    fix: alter created_at field in transactions

--- db/migrations/20240910141739_finally-alter-created_at-transaction-field.ts
+++ db/migrations/20240910141739_finally-alter-created_at-transaction-field.ts
@@ -2,11 +2,7 @@ import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('transactions', (table) => {
-    table.timestamp('created_at').nullable().alter()
-  })
-
-  await knex.schema.alterTable('transactions', (table) => {
-    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable().alter()
+    table.timestamp('created_at').alter().defaultTo(knex.fn.now()).notNullable()
   })
 }
 
